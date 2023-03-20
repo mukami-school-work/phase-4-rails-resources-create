ada8edbfdc59dbaafafb11224ad44c0d1069faa5
lizdmukami <lizdmukami@gmail.com>
2023-03-20 17:56:02 +0300

    Completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,6 @@
 class BirdsController < ApplicationController
 
+  # Wrap_parameters format: []
   # GET /birds
   def index
     birds = Bird.all
@@ -15,5 +16,17 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
+  # POST /birds
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
+
+  # private
+
+  # def bird_params
+  #   params.permit(:name, :species)
+  # end
 
 end
